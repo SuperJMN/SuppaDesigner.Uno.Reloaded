1de5ee540921af28770a8bb351a50d0687ee3c9b
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-09-24 10:24:21 +0200

    Add project files.

---
+++ MyUno/MyUno.Shared/Zafiro/PropertyEditor/Editor.cs
@@ -0,0 +1,10 @@
+﻿using Windows.UI.Xaml;
+
+namespace MyUno.Zafiro.PropertyEditor
+{
+    public class Editor
+    {
+        public DataTemplate Template { get; set; }
+        public EditorKey Key { get; set; }
+    }
+}
---
+++ MyUno/MyUno.Shared/Zafiro/PropertyEditor/EditorCollection.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MyUno.Zafiro.PropertyEditor
+{
+    public class EditorCollection : Collection<Editor>
+    {
+    }
+}
